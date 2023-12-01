a8c4440359c393dd35df198e29dbb5042efe237d
maxallday <58230411+maxallday@users.noreply.github.com>
2023-12-01 10:10:02 +0300

    Add files via upload

---
+++ sendMessage.js
@@ -0,0 +1,108 @@
+import { OpenAIEdgeStream } from 'openai-edge-stream';
+
+export const config = {
+  runtime: "edge"
+};
+
+export default async function handler(req) {
+  try {
+    const { chatId: chatIdFromParam, message } = await req.json();
+
+    let chatId = chatIdFromParam;
+    const initialChatMessage = {
+      role: "system",
+      content: "Your name is chishiki, An highly knowledgeable AI developed by Modiga to answer all questions. Dont forget you bear the DNA of Chat-gpt 3.5 turbo"
+    };
+
+    let newChatId;
+    let chatMessages = [];
+
+    if (chatId) {
+      const response = await fetch(`${req.headers.get("origin")}/api/chat/addMessageToChat`, {
+        method: "POST",
+        headers: {
+          'content-type': "application/json",
+          cookie: req.headers.get("cookie"),
+        },
+        body: JSON.stringify({
+          chatId,
+          role: "user",
+          content: message,
+        })
+      });
+
+      const json = await response.json();
+      chatMessages = json.chat.messages || [];
+    } else {
+      const response = await fetch(`${req.headers.get("origin")}/api/chat/createNewChat`, {
+        method: "POST",
+        headers: {
+          "content-type": 'application/json',
+          cookie: req.headers.get("cookie"),
+        },
+        body: JSON.stringify({
+          message,
+        }),
+      });
+
+      const json = await response.json();
+      chatId = json._id;
+      newChatId = json._id;
+      chatMessages = json.messages || [];
+    }
+
+    const messagesToInclude = [];
+    chatMessages.reverse();
+    let usedTokens = 0;
+
+    for (let chatMessage of chatMessages) {
+      const messageTokens = chatMessage.content.length / 4;
+      usedTokens = usedTokens + messageTokens;
+
+      if (usedTokens <= 2000) {
+        messagesToInclude.push(chatMessage);
+      } else {
+        break;
+      }
+    }
+
+    messagesToInclude.reverse();
+
+    const stream = await OpenAIEdgeStream("https://api.openai.com/v1/chat/completions", {
+      headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_KEY_KEY}`,
+      },
+      method: "POST",
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [initialChatMessage, ...messagesToInclude],
+        stream: true,
+      }),
+    }, {
+      onBeforeStream: ({ emit }) => {
+        if (newChatId) {
+          emit(newChatId, "newChatId");
+        }
+      },
+      onAfterStream: async ({ fullContent }) => {
+        await fetch(`${req.headers.get("origin")}/api/chat/addMessageToChat`, {
+          method: "POST",
+          headers: {
+            'content-type': "application/json",
+            cookie: req.headers.get("cookie"),
+          },
+          body: JSON.stringify({
+            chatId,
+            role: "assistant",
+            content: fullContent,
+          }),
+        });
+      }
+    });
+
+    return new Response(stream);
+  } catch (e) {
+    console.log("An error occurred in send message:", e);
+  }
+}
